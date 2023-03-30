321f3633872fce04d238f429e5cba75dce99c40d
Leo Blanco <leopoldoblanco@gmail.com>
2023-03-30 14:22:16 -0300

    adds TypeScript

--- lib/posts.ts
+++ lib/posts.ts
@@ -16,7 +16,7 @@ export function getSortedPostsData() {
 
     return {
       id,
-      ...matterResult.data
+      ...matterResult.data as { date: string; title: string }
     }
   });
 
@@ -41,7 +41,7 @@ export function getAllPostIds() {
   ));
 }
 
-export async function getPostData(id) {
+export async function getPostData(id: string) {
   const fullPath = path.join(postsDirectory, `${id}.md`);
   const fileContents = fs.readFileSync(fullPath, 'utf8');
 
@@ -55,6 +55,6 @@ export async function getPostData(id) {
   return {
     id,
     contentHtml,
-    ...matterResult.data,
+    ...matterResult.data as { date: string; title: string },
   };
 }
